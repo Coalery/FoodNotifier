a34e82f3fbeb8bc47b2b91dcdca3b6caa16bca56
Coalery <doralife12@naver.com>
2020-08-21 01:26:14 +0900

    Add: DBHelper#getRecipes

--- food_notifier/lib/db_helper.dart
+++ food_notifier/lib/db_helper.dart
@@ -1,11 +1,28 @@
+import 'dart:convert';
+
 import 'package:food_notifier/food.dart';
+import 'package:food_notifier/recipe.dart';
 import 'package:http/http.dart' as http;
 
 class DBHelper {
+  static const String IP = '52.78.203.43';
+
   Future<Food> getFood(String barcode) async {
-    http.Response response = await http.get('http://13.124.188.74:3000/barcode/' + barcode);
+    http.Response response = await http.get('http://$IP:3000/barcode/' + barcode);
     String body = response.body;
 
     return Food.fromJsonString(body.substring(1, body.length - 1));
+  }
+
+  Future<List<Recipe>> getRecipes(String ingredient) async {
+    http.Response response = await http.get('http://$IP:3000/recipe/' + ingredient);
+    JsonCodec codec = new JsonCodec();
+
+    List<dynamic> jsonArray = List.from(codec.decode(response.body));
+    List<Recipe> resultList = [];
+    for(dynamic json in jsonArray) {
+      resultList.add(Recipe.fromJson(json));
+    }
+    return resultList;
   }
 }
