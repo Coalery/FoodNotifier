979b7074ca2def6a19503a9a4d89345c9d8cdc97
Coalery <doralife12@naver.com>
2020-08-26 18:31:11 +0900

    Add: Recipe page

--- food_notifier/lib/main.dart
+++ food_notifier/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:food_notifier/main_page.dart';
+import 'package:food_notifier/recipe_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
         '/' : (context) => MainPage(),
-        MainPage.routeName : (context) => MainPage()
+        MainPage.routeName : (context) => MainPage(),
+        RecipePage.routeName : (context) => RecipePage()
       },
     );
   }
---
+++ food_notifier/lib/recipe_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:food_notifier/bar_widget.dart';
+import 'package:food_notifier/recipe.dart';
+
+class RecipePageArguments {
+  Recipe recipe;
+  RecipePageArguments(this.recipe);
+}
+
+class RecipePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    RecipePageArguments arguments = ModalRoute.of(context).settings.arguments;
+    Recipe recipe = arguments.recipe;
+
+    return Scaffold(
+      body: SafeArea(
+        child: BarWidget(
+          title: recipe.f_RCP_NM,
+          child: Container(
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
