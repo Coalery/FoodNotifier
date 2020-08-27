9567dd73db3e85f0af313190d06efaabf6760cba
Coalery <doralife12@naver.com>
2020-08-27 15:12:04 +0900

    Add: Dummy food contents

--- food_notifier/lib/dummy/dummy_food.dart
+++ food_notifier/lib/dummy/dummy_food.dart
@@ -1,5 +1,9 @@
 class DummyFood {
   static List<String> jsons = [
-    
+    '{"BAR_CD":"8802039211424","PRDLST_NM":"면사랑 구수한 메밀국수","PRDLST_DCNM":"건면","PRMS_DT":"20070119","BSSH_NM":"(주)면사랑","CLSBIZ_DT":"","INDUTY_NM":"식품제조가공업","SITE_ADDR":"충청북도 진천군 이월면 궁동길 51-24","POG_DAYCNT":"24개월  실온","END_DT":"","PRDLST_REPORT_NO":"19930443028350"}',
+    '{"BAR_CD":"8801696003397","PRDLST_NM":"종합캔디","PRDLST_DCNM":"캔디류","PRMS_DT":"20000110","BSSH_NM":"(주)한일제과","CLSBIZ_DT":"","INDUTY_NM":"식품제조가공업","SITE_ADDR":"경기도 광주시 독고개길 156","POG_DAYCNT":"제조일로부터18개월","END_DT":"","PRDLST_REPORT_NO":"1987036804531"}',
+    '{"BAR_CD":"8805522061018","PRDLST_NM":"좋은상품 유기농 부침가루","PRDLST_DCNM":"기타가공품","PRMS_DT":"20090724","BSSH_NM":"사조동아원(주) 부산공장","CLSBIZ_DT":"","INDUTY_NM":"식품제조가공업","SITE_ADDR":"부산광역시 동구 충장대로 293(좌천동)","POG_DAYCNT":"제조일로부터 12개월까지","END_DT":"20200217","PRDLST_REPORT_NO":"19950129282236"}',
+    '{"BAR_CD":"8801649120355","PRDLST_NM":"쵸코카라멜","PRDLST_DCNM":"캔디류","PRMS_DT":"19971009","BSSH_NM":"한양제과주식회사","CLSBIZ_DT":"","INDUTY_NM":"식품제조가공업","SITE_ADDR":"경기도 동두천시 평화로2862번길 15(상봉암동)","POG_DAYCNT":"제조일로부터 12개월","END_DT":"20161124","PRDLST_REPORT_NO":"1997031009828"}',
+    '{"BAR_CD":"8809359120061","PRDLST_NM":"풍기홍삼愛홍삼정골드","PRDLST_DCNM":"액상차","PRMS_DT":"20120420","BSSH_NM":"풍기홍삼애제조원","CLSBIZ_DT":"","INDUTY_NM":"식품제조가공업","SITE_ADDR":"경상북도 영주시 소백로1916번길 34","POG_DAYCNT":"제조일로부터 2년까지(실온)","END_DT":"","PRDLST_REPORT_NO":"201205540864"}'
   ];
 }
