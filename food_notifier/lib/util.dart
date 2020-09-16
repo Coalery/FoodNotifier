ec15264bd14d93d21e68588f738c7e03adb90b13
Coalery <doralife12@naver.com>
2020-09-16 18:06:37 +0900

    Add: DateTime - String Parser

---
+++ food_notifier/lib/util.dart
@@ -0,0 +1,16 @@
+import 'package:intl/intl.dart';
+
+DateTime parseStringToDateTime(String val) {
+  int year = int.parse(val.substring(0, 4));
+  int month = int.parse(val.substring(4, 6));
+  int day = int.parse(val.substring(6, 8));
+  int hour = int.parse(val.substring(8, 10));
+  int minute = int.parse(val.substring(10, 12));
+
+  return DateTime(year, month, day, hour, minute);
+}
+
+String parseDateTimeToString(DateTime val) {
+  DateFormat formatter = new DateFormat("yyyyMMddHHmm");
+  return formatter.format(val);
+}
